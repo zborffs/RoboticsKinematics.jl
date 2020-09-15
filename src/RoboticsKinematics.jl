91621c4960fba5a1c7753f90245097996c7050ac
Zach Bortoff <zbortoff@gmail.com>
2020-09-14 21:56:20 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/RoboticsKinematics.jl
@@ -0,0 +1,5 @@
+module RoboticsKinematics
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RoboticsKinematics
+using Test
+
+@testset "RoboticsKinematics.jl" begin
+    # Write your tests here.
+end
